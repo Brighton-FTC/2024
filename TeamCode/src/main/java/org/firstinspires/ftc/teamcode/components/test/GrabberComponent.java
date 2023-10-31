79e4caa8020a858b1e11e0bf40dfef572942a322
Saku <saku.bottingota@gmail.com>
2023-10-31 18:19:52 +0000

    Changed grabber code to use SimpleServo class, as stated in the FTCLib docs, and rejigged controls.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/components/test/GrabberComponent.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/components/test/GrabberComponent.java
@@ -1,8 +1,10 @@
 package org.firstinspires.ftc.teamcode.components.test;
 
 import com.arcrobotics.ftclib.gamepad.GamepadEx;
+import com.arcrobotics.ftclib.gamepad.GamepadKeys;
 import com.arcrobotics.ftclib.gamepad.GamepadKeys.Button;
 import com.arcrobotics.ftclib.hardware.ServoEx;
+import com.arcrobotics.ftclib.hardware.SimpleServo;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -11,8 +13,8 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
  * Code to open/close grabber, and tilt grabber. <br />
  * Controls:
  * <ul>
- *     <li>Open/close grabber: A (Cross on PlayStation controller)</li>
- *     <li>Tilt grabber up/down: B (Circle on PlayStation controller)</li>
+ *     <li>Open/close grabber: Right bumper</li>
+ *     <li>Tilt grabber up: Right lever</li>
  * </ul>
  */
 
@@ -22,57 +24,49 @@ public class GrabberComponent extends OpMode {
     // TODO: fill in these values
     public final int GRABBER_CLOSED_POSITION = 0;
     public final int GRABBER_OPEN_POSITION = 90;
-    public final int GRABBER_TILT_DOWN_POSITION = 0;
-    public final int GRABBER_TILT_UP_POSITION = 180;
+    public final int MIN_GRABBER_TILT_POSITION = 0;
+    public final int MAX_GRABBER_TILT_POSITION = 90;
 
     private ServoEx grabberServo;
     private ServoEx grabberTiltServo;
 
-    private boolean isGrabberClosed = false;
-    private boolean isGrabberTiltedUp = false;
+    private boolean isGrabberClosed = true;
+    // private boolean isGrabberTiltedDown = true;
 
     private GamepadEx gamepad = new GamepadEx(gamepad1);
 
     @Override
     public void init() {
         // TODO: fill in device names
-        grabberServo = (ServoEx) hardwareMap.servo.get("device name here");
-        grabberTiltServo = (ServoEx) hardwareMap.servo.get("device name here as well");
-
-        // set ranges on servos, just in case
-        grabberServo.setRange(GRABBER_CLOSED_POSITION, GRABBER_OPEN_POSITION);
-        grabberTiltServo.setRange(GRABBER_TILT_DOWN_POSITION, GRABBER_TILT_UP_POSITION);
+        grabberServo = new SimpleServo(hardwareMap, "servo_name", GRABBER_CLOSED_POSITION, GRABBER_OPEN_POSITION);
+        grabberTiltServo = new SimpleServo(hardwareMap, "servo_name", MIN_GRABBER_TILT_POSITION, MAX_GRABBER_TILT_POSITION);
     }
 
-    // I don't know if this is necessary, but I put it in here for now
     @Override
     public void start() {
         // set servos to their starting positions
         grabberServo.setPosition(GRABBER_OPEN_POSITION);
-        grabberTiltServo.setPosition(GRABBER_TILT_DOWN_POSITION);
+        grabberTiltServo.setPosition(MIN_GRABBER_TILT_POSITION);
     }
 
     @Override
     public void loop() {
-        if (gamepad.wasJustPressed(Button.A)) {
+        if (gamepad.wasJustPressed(Button.RIGHT_BUMPER)) {
             if (isGrabberClosed) {
                 grabberServo.setPosition(GRABBER_OPEN_POSITION);
             } else {
-                grabberServo.setPosition(GRABBER_OPEN_POSITION);
+                grabberServo.setPosition(GRABBER_CLOSED_POSITION);
             }
 
             isGrabberClosed = !isGrabberClosed;
         }
 
-        if (gamepad.wasJustPressed(Button.B)) {
-            if (isGrabberTiltedUp) {
-                grabberTiltServo.setPosition(GRABBER_TILT_DOWN_POSITION);
-            } else {
-                grabberTiltServo.setPosition(GRABBER_TILT_UP_POSITION);
-            }
-
-            isGrabberTiltedUp = !isGrabberTiltedUp;
+        if (gamepad.getTrigger(GamepadKeys.Trigger.RIGHT_TRIGGER) > 0.5) {
+            grabberTiltServo.setPosition(MAX_GRABBER_TILT_POSITION);
+        } else {
+            grabberTiltServo.setPosition(MIN_GRABBER_TILT_POSITION);
         }
+
 
         telemetry.addData("Grabber Position", isGrabberClosed ? "Closed" : "Open");
         telemetry.addData("Grabber Tilt Angle", grabberTiltServo.getAngle());
