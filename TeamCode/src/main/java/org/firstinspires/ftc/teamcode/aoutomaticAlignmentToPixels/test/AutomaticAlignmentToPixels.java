ed943e703a8d1d6f59c52e1ee0b5239a04403bf0
Saku <saku.bottingota@gmail.com>
2023-11-04 14:14:30 +0000

    Made a code to get the input of the desired stack of pixels and (hopefully) make the robot drive straight to the desired stack of pixels.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/aoutomaticAlignmentToPixels/test/AutomaticAlignmentToPixels.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/aoutomaticAlignmentToPixels/test/AutomaticAlignmentToPixels.java
@@ -3,8 +3,6 @@ package org.firstinspires.ftc.teamcode.aoutomaticAlignmentToPixels.test;
 import com.arcrobotics.ftclib.drivebase.MecanumDrive;
 import com.arcrobotics.ftclib.gamepad.GamepadEx;
 import com.arcrobotics.ftclib.gamepad.GamepadKeys;
-import com.arcrobotics.ftclib.hardware.SensorDistanceEx;
-import com.arcrobotics.ftclib.hardware.SensorRevTOFDistance;
 import com.arcrobotics.ftclib.hardware.motors.Motor;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
@@ -12,7 +10,6 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.util.Range;
 
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
-import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import org.firstinspires.ftc.vision.VisionPortal;
 import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
 import org.firstinspires.ftc.vision.apriltag.AprilTagPoseFtc;
@@ -22,25 +19,31 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 /**
- * Prototype for aligning robot to a stack of pixels (<b>untested and in developement</b>). <br />
- * It's meant to go to an april tag when an input is given, and then strafe until another input is given. <br />
+ * Prototype for aligning robot to a stack of pixels (<b>untested and in development</b>). <br />
+ * It's meant to go to a specified pixel stack when an input is given <br />
+ * <i>Warning: make sure that the robot is angled towards the left april tag for the first 3 pixel stacks, and the right april tag for the last 3.
+ * Otherwise, the robot will inevitably crash into a wall. </i>
+ *
+ * <hr />
  *
  * Controls:
  * <ul>
- *     <li>X - Start going to an april tag (the leftmost one if there are more than one onscreen) and then strafe right until X is pressed again. </li>
- *     <li>Y - Start going to april tag (the rightmost one if there are more than one onscreen) and then strafe left until Y pressed again. </li>
+ *     <li>X - Start going to a cone stack (press again to cancel). </li>
+ *     <li>DPAD LEFT and DPAD RIGHT - change the pixel stack that the robot is meant to be going to. </li>
  * </ul>
+ *
+ * <hr />
  *
  * Stuff to do:
  * <ul>
- *     <li>Implement a way to input the desired stack of pixels to go to. </li>
- *     <li>Find way to make robot go directly to the specified stack of pixels. </li>
- *     <li>Get the distance from each april tag to each stack of pixels, and put it in the code to offset the robot from the april tags. </li>
+ *     <li>Implement a better way to input the desired stack of pixels to go to. </li>
+ *     <li>See if the current code rotates the robot towards the april tag instead of towards the wall, and if so find a better way to move the robot</li>
  *     <li>Find a better way to decide which april tag to use if there are more than one onscreen. </li>
+ *     <li>Find a way to make the robot go to the right pixel stack regardless of whether it's pointing at the left or right april tags. </li>
+ *     <li>Maybe covert the opmode to a linear opmode, as I think it would be neater. </li>
  *     <li>Find out and fill in the component names. </li>
  *     <li>Test and fine tune the DESIRED_DISTANCE, SPEED/STRAFE/TURN_GAIN and MAX_AUTO_SPEED/STRAFE/TURN constants. </li>
  *     <li>Make the telemetry data clearer. </li>
- *     <li>Find a way to get rid of the inches and replace them with metric units</li>
  * </ul>
  */
 
@@ -63,13 +66,16 @@ public class AutomaticAlignmentToPixels extends OpMode {
 
     private MecanumDrive mecanum;
 
-    private SensorDistanceEx distanceSensor;
+    // private SensorDistanceEx distanceSensor;
+    // private GyroEx gyro;
 
     private GamepadEx gamepad = new GamepadEx(gamepad1);
 
+    private final int N_PIXEL_STACKS = 6;
+    private final double[] xOffsetFromAprilTags = {0, 11, 22, -22, -11, 0}; // 11 inch spacing between stacks
+    private int selectedPixelStack = 0;
+
     private boolean isLookingForAprilTags = false;
-    private boolean isStrafeDirectionRight;
-    private boolean isStrafing = false;
 
     @Override
     public void init() {
@@ -82,62 +88,39 @@ public class AutomaticAlignmentToPixels extends OpMode {
                 .addProcessor(aprilTag)
                 .build();
 
-        mecanum  = new MecanumDrive(
+        mecanum = new MecanumDrive(
                 new Motor(hardwareMap, "front_left"),
                 new Motor(hardwareMap, "front_right"),
                 new Motor(hardwareMap, "back_left"),
                 new Motor(hardwareMap, "back_right")
         );
 
-        distanceSensor = new SensorRevTOFDistance(hardwareMap, "distance_sensor_name");
+        // distanceSensor = new SensorRevTOFDistance(hardwareMap, "distance_sensor_name");
+        // gyro = new RevIMU(hardwareMap, "gyro_name");
 
-        gamepad.getGamepadButton(GamepadKeys.Button.X).whenPressed(() -> {
-            if (isStrafing) {
-                isStrafing = false;
-            } else {
-                isLookingForAprilTags = !isLookingForAprilTags;
-            }
+        gamepad.getGamepadButton(GamepadKeys.Button.DPAD_LEFT).whenPressed(() -> selectedPixelStack = selectedPixelStack + 1 % N_PIXEL_STACKS);
+        gamepad.getGamepadButton(GamepadKeys.Button.DPAD_RIGHT).whenPressed(() -> selectedPixelStack = selectedPixelStack - 1 % N_PIXEL_STACKS);
 
-            isStrafeDirectionRight = true;
-        });
-
-        gamepad.getGamepadButton(GamepadKeys.Button.Y).whenPressed(() -> {
-            if (isStrafing) {
-                isStrafing = false;
-            } else {
-                isLookingForAprilTags = !isLookingForAprilTags;
-            }
-
-            isStrafeDirectionRight = false;
-        });
+        gamepad.getGamepadButton(GamepadKeys.Button.X).whenPressed(() -> isLookingForAprilTags = !isLookingForAprilTags);
     }
 
     @Override
     public void loop() {
+        telemetry.addData("Pixel stack selected", selectedPixelStack);
+
         if (isLookingForAprilTags) {
             telemetry.addLine("== Robot is detecting april tags ==");
 
-            AprilTagDetection aprilTagDetection = getAprilTag(isStrafeDirectionRight);
+            AprilTagDetection aprilTagDetection = getAprilTag();
 
             if (aprilTagDetection != null) {
-                boolean hasStopped = moveToPosition(aprilTagDetection.ftcPose);
+                boolean hasStopped = moveToPosition(aprilTagDetection.ftcPose, xOffsetFromAprilTags[selectedPixelStack]);
 
                 if (hasStopped) {
-                    isStrafing = true;
                     isLookingForAprilTags = false;
                 }
-            }
-        }
-
-        if (isStrafing) {
-            if (isStrafeDirectionRight) {
-                telemetry.addLine("== Robot is strafing right ==");
-
-                mecanum.driveRobotCentric(MAX_AUTO_STRAFE, 0, 0);
             } else {
-                telemetry.addLine("== Robot is strafing left ==");
-
-                mecanum.driveRobotCentric(-MAX_AUTO_STRAFE, 0, 0);
+                isLookingForAprilTags = false;
             }
         }
     }
@@ -148,8 +131,8 @@ public class AutomaticAlignmentToPixels extends OpMode {
     }
 
     // gets the april tag that the robot should use for its pathfinding
-    // if there are more than one april tags on the screen, then it will detect the leftmost one if isStrafeDirectionRight is true, otherwise the rightmost one
-    private AprilTagDetection getAprilTag(boolean isStrafeDirectionRight) {
+    // if there are more than one april tags on the screen, then it will chose the first one returned
+    private AprilTagDetection getAprilTag() {
         // get the detections and filter out the ones without metadata
         List<AprilTagDetection> aprilTagDetections = aprilTag.getDetections()
                 .stream()
@@ -159,50 +142,28 @@ public class AutomaticAlignmentToPixels extends OpMode {
         if (aprilTagDetections.size() == 0) {
             telemetry.addLine("\tNo april tags detected");
             return null;
-        } else if (aprilTagDetections.size() == 1) {
-            telemetry.addLine("\t1 april tag detected");
-            return aprilTagDetections.get(0);
         } else {
             telemetry.addLine("\t" + aprilTagDetections.size() + "april tags detected");
-
-            // TODO: figure out if there's a better way to do this
-            AprilTagDetection returnedDetection = aprilTagDetections.get(0);
-            for (int i = 1; i < aprilTagDetections.size(); i++) {
-                if (isStrafeDirectionRight) { // go to the left and strafe right
-                    if (aprilTagDetections.get(i).ftcPose.x > returnedDetection.ftcPose.x) {
-                        returnedDetection = aprilTagDetections.get(i);
-                    }
-                } else { // go to the right and strafe left
-                    if (aprilTagDetections.get(i).ftcPose.x < returnedDetection.ftcPose.x) {
-                        returnedDetection = aprilTagDetections.get(i);
-                    }
-                }
-            }
-            return returnedDetection;
+            return aprilTagDetections.get(0); // TODO: if multiple april tags is a problem, replace this with something more rigorous
         }
     }
 
     // attempts to move robot to april tag, and returns if the robot has stopped
-    // TODO: figure out a way to make the robot go directly to the desired stack, using xOffset
-    private boolean moveToPosition(AprilTagPoseFtc anchor) { //, double xOffset) {
-        if (distanceSensor.getDistance(DistanceUnit.INCH) > DESIRED_DISTANCE) {
-            telemetry.addLine("== Robot is moving to april tag ==");
-            telemetry.addData("\tDistance to wall (meters)", distanceSensor.getDistance(DistanceUnit.METER));
+    private boolean moveToPosition(AprilTagPoseFtc anchor, double xOffset) {
+        telemetry.addLine("== Robot is moving to april tag ==");
 
-            // calculate how far the robot has to go to be in the desired position
-            double  rangeError = anchor.range - DESIRED_DISTANCE;
-            double  headingError = anchor.bearing;
-            double  yawError = anchor.yaw;
+        // calculate how far the robot has to go to be in the desired position
+        double  rangeError = anchor.range - DESIRED_DISTANCE;
+        double  headingError = anchor.bearing;
+        double  yawError = anchor.yaw + xOffset;
 
-            // calculate how the robot should move
-            double drive  = Range.clip(rangeError * SPEED_GAIN, -MAX_AUTO_SPEED, MAX_AUTO_SPEED);
-            double turn   = Range.clip(headingError * TURN_GAIN, -MAX_AUTO_TURN, MAX_AUTO_TURN) ;
-            double strafe = Range.clip(-yawError * STRAFE_GAIN, -MAX_AUTO_STRAFE, MAX_AUTO_STRAFE);
-            mecanum.driveRobotCentric(strafe, drive, turn);
+        // calculate how the robot should move
+        // TODO: if this code makes the robot turn towards the april tag, not towards the wall, figure out a way of fixing that
+        double drive = Range.clip(rangeError * SPEED_GAIN, -MAX_AUTO_SPEED, MAX_AUTO_SPEED);
+        double turn = Range.clip(headingError * TURN_GAIN, -MAX_AUTO_TURN, MAX_AUTO_TURN);
+        double strafe = Range.clip(-yawError * STRAFE_GAIN, -MAX_AUTO_STRAFE, MAX_AUTO_STRAFE);
+        mecanum.driveRobotCentric(strafe, drive, turn);
 
-            return false;
-        } else {
-            return true;
-        }
+        return anchor.range <= DESIRED_DISTANCE; // TODO: figure out a better way to tell when the robot has stopped
     }
 }
