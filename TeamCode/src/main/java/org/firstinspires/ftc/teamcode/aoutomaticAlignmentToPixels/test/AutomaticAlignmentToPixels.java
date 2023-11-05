520dfd22edab566be3bd581bba07c86172bf0d0c
Saku <saku.bottingota@gmail.com>
2023-11-05 10:16:43 +0000

    Made the robot look at the april tag once, and then (hopefully) set its angle towards the correct stack and drive a certain distance, using the gyro and the distance sensor.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/aoutomaticAlignmentToPixels/test/AutomaticAlignmentToPixels.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/aoutomaticAlignmentToPixels/test/AutomaticAlignmentToPixels.java
@@ -5,13 +5,17 @@ import androidx.annotation.NonNull;
 import com.arcrobotics.ftclib.drivebase.MecanumDrive;
 import com.arcrobotics.ftclib.gamepad.GamepadEx;
 import com.arcrobotics.ftclib.gamepad.GamepadKeys;
+import com.arcrobotics.ftclib.hardware.GyroEx;
+import com.arcrobotics.ftclib.hardware.RevIMU;
+import com.arcrobotics.ftclib.hardware.SensorDistanceEx;
+import com.arcrobotics.ftclib.hardware.SensorRevTOFDistance;
 import com.arcrobotics.ftclib.hardware.motors.Motor;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.util.Range;
 
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import org.firstinspires.ftc.vision.VisionPortal;
 import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
 import org.firstinspires.ftc.vision.apriltag.AprilTagPoseFtc;
@@ -40,11 +44,12 @@ import java.util.stream.Collectors;
  * <ul>
  *     <li>Find a better way of knowing when the robot has arrived in front of the desired pixel stack. </li>
  *     <li>Implement a better way to input the desired stack of pixels to go to. </li>
+ *     <li>Make the robot turn perpendicular towards the wall when it arrives. </li>
  *     <li>Find a better way to decide which april tag to use if there are more than one onscreen. </li>
  *     <li>Find a way to make the robot go to the right pixel stack regardless of whether it's pointing at the left or right april tags. </li>
  *     <li>Clear neaten up the linear opmode. </li>
  *     <li>Find out and fill in the component names. </li>
- *     <li>Test and fine tune the DESIRED_DISTANCE, SPEED/STRAFE/TURN_GAIN and MAX_AUTO_SPEED/STRAFE/TURN constants. </li>
+ *     <li>Test and fine tune the DISTANCE_ERROR, ANGLE_ERROR, DESIRED_DISTANCE, MAX_AUTO_SPEED and MAX_AUTO_TURN constants. </li>
  *     <li>Make the telemetry data clearer. </li>
  * </ul>
  */
@@ -53,14 +58,12 @@ import java.util.stream.Collectors;
 @TeleOp(name = "Automatic Alignment To Pixels", group = "operation-valour-test")
 public class AutomaticAlignmentToPixels extends LinearOpMode {
     // TODO: fine tune these by testing
+    private final double DISTANCE_ERROR = 6; // in inches
+    private final double ANGLE_ERROR = 20; // in degrees
+
     private final double DESIRED_DISTANCE = 60; // in inches
 
-    private final double SPEED_GAIN = 0.02;
-    private final double STRAFE_GAIN = 0.015;
-    private final double TURN_GAIN = 0.01;
-
     private final double MAX_AUTO_SPEED = 0.5;
-    private final double MAX_AUTO_STRAFE = 0.5;
     private final double MAX_AUTO_TURN = 0.3;
 
     private AprilTagProcessor aprilTag;
@@ -68,8 +71,8 @@ public class AutomaticAlignmentToPixels extends LinearOpMode {
 
     private MecanumDrive mecanum;
 
-    // private SensorDistanceEx distanceSensor;
-    // private GyroEx gyro;
+     private SensorDistanceEx distanceSensor;
+     private GyroEx gyro;
 
     private GamepadEx gamepad = new GamepadEx(gamepad1);
 
@@ -97,34 +100,40 @@ public class AutomaticAlignmentToPixels extends LinearOpMode {
                 new Motor(hardwareMap, "back_right")
         );
 
-        // distanceSensor = new SensorRevTOFDistance(hardwareMap, "distance_sensor_name");
-        // gyro = new RevIMU(hardwareMap, "gyro_name");
+        distanceSensor = new SensorRevTOFDistance(hardwareMap, "distance_sensor_name");
+        gyro = new RevIMU(hardwareMap, "gyro_name");
 
-        gamepad.getGamepadButton(GamepadKeys.Button.DPAD_LEFT).whenPressed(() -> selectedPixelStack = selectedPixelStack + 1 % N_PIXEL_STACKS);
-        gamepad.getGamepadButton(GamepadKeys.Button.DPAD_RIGHT).whenPressed(() -> selectedPixelStack = selectedPixelStack - 1 % N_PIXEL_STACKS);
+        gamepad.getGamepadButton(GamepadKeys.Button.DPAD_LEFT).whenPressed(() -> selectedPixelStack = (selectedPixelStack + 1) % N_PIXEL_STACKS);
+        gamepad.getGamepadButton(GamepadKeys.Button.DPAD_RIGHT).whenPressed(() -> selectedPixelStack = (selectedPixelStack - 1) % N_PIXEL_STACKS);
 
         gamepad.getGamepadButton(GamepadKeys.Button.X).whenPressed(() -> isLookingForAprilTags = !isLookingForAprilTags);
+
+
+        AprilTagDetection aprilTagDetection = null;
 
         waitForStart();
 
         while (opModeIsActive()) {
             telemetry.addData("Pixel stack selected", selectedPixelStack);
 
+
             if (isLookingForAprilTags) {
                 telemetry.addLine("== Robot is detecting april tags ==");
 
-                AprilTagDetection aprilTagDetection = getAprilTag();
+                aprilTagDetection = getAprilTag();
+                gyro.reset();
 
-                if (aprilTagDetection != null) {
+            }
 
-                    boolean hasStopped = moveToPosition(aprilTagDetection.ftcPose, xOffsetFromAprilTags[selectedPixelStack]);
+            if (aprilTagDetection != null) {
+                isLookingForAprilTags = false;
 
-                    if (hasStopped) {
-                        isLookingForAprilTags = false;
-                    }
-                } else {
-                    isLookingForAprilTags = false;
+                boolean hasStopped = moveToPosition(aprilTagDetection.ftcPose, xOffsetFromAprilTags[selectedPixelStack]);
+
+                if (hasStopped) {
+                    aprilTagDetection = null;
                 }
+
             }
 
             sleep(10);
@@ -155,18 +164,41 @@ public class AutomaticAlignmentToPixels extends LinearOpMode {
     private boolean moveToPosition(@NonNull AprilTagPoseFtc anchor, double xOffset) {
         telemetry.addLine("== Robot is moving to april tag ==");
 
-        // calculate how far the robot has to go to be in the desired position
-        double  rangeError = anchor.range - DESIRED_DISTANCE;
-        double  headingError = Math.atan2(anchor.y, anchor.x + xOffset);
-        double  yawError = anchor.yaw + xOffset;
+        // calculate how far the robot has to turn/move
+        // idk if I have to use anchor.y or anchor.range here, but am using anchor.y for now
+        double  headingError = Math.atan2(anchor.y, anchor.x + xOffset) * 180/Math.PI;
+        double distanceError = distanceSensor.getDistance(DistanceUnit.INCH);
 
-        // calculate how the robot should move
-        // TODO: if this code makes the robot turn towards the april tag, not towards the wall, figure out a way of fixing that
-        double drive = Range.clip(rangeError * SPEED_GAIN, -MAX_AUTO_SPEED, MAX_AUTO_SPEED);
-        double turn = Range.clip(headingError * TURN_GAIN, -MAX_AUTO_TURN, MAX_AUTO_TURN);
-        double strafe = Range.clip(-yawError * STRAFE_GAIN, -MAX_AUTO_STRAFE, MAX_AUTO_STRAFE);
-        mecanum.driveRobotCentric(strafe, drive, turn);
+        double currentHeading = gyro.getHeading();
+        // set the angle to be from -179 to 180 instead of 0 to 359
+        if (currentHeading > 180) {
+            currentHeading -= 360;
+        }
 
-        return anchor.range <= DESIRED_DISTANCE; // TODO: figure out a better way to tell when the robot has stopped
+        double currentDistance = distanceSensor.getDistance(DistanceUnit.INCH);
+
+        if (!(Math.abs(headingError - gyro.getHeading()) <= ANGLE_ERROR)) {
+            telemetry.addLine("\tTurning to correct angle");
+
+            if (currentHeading < 0) {
+                mecanum.driveRobotCentric(0, 0, MAX_AUTO_TURN);
+            } else {
+                mecanum.driveRobotCentric(0, 0, -MAX_AUTO_TURN);
+            }
+
+        } else if (!(Math.abs(distanceError - DESIRED_DISTANCE) <= DISTANCE_ERROR)){
+            telemetry.addLine("\tMoving to correct distance");
+
+            if (currentDistance > DESIRED_DISTANCE) {
+                mecanum.driveRobotCentric(0, MAX_AUTO_SPEED, 0);
+            } else {
+                mecanum.driveRobotCentric(0, -MAX_AUTO_SPEED, 0);
+            }
+
+        } else {
+            return true;
+        }
+
+        return false;
     }
 }
