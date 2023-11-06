82c8bdd0cab9cb8330a154a55e5a602d2ee4a4c1
Saku <saku.bottingota@gmail.com>
2023-11-06 18:51:58 +0000

    Realised I forgot half of the AutomaticAlignmentToPixelsTestOpMode code, so put it in.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/aoutomaticAlignmentToPixels/test/AutomaticAlignmentToPixelsTestOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/aoutomaticAlignmentToPixels/test/AutomaticAlignmentToPixelsTestOpMode.java
@@ -12,12 +12,15 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
  *
  * Controls:
  * <ul>
- *     <li>A (Cross on PlayStation) - Start/stop the automatic alignment to pixels</li>
+ *     <li>A (Cross on PlayStation) - Start/stop the automatic alignment to pixels. </li>
+ *     <li>DPAD left/right - Change the selected pixel stack. </li>
  * </ul>
  */
 @Disabled
 @TeleOp(name = "Automatic Alignment To Pixels Test OpMode", group = "operation-valour-test")
 public class AutomaticAlignmentToPixelsTestOpMode extends OpMode {
+    public final int N_PIXEL_TAGS = AutomaticAlignmentToPixels.N_PIXEL_STACKS;
+
     private GamepadEx gamepad = new GamepadEx(gamepad1);
 
     private int selectedPixelStack = 0;
@@ -33,11 +36,17 @@ public class AutomaticAlignmentToPixelsTestOpMode extends OpMode {
                 AutomaticAlignmentToPixels.stopMoving();
             }
         });
+
+        gamepad.getGamepadButton(GamepadKeys.Button.DPAD_LEFT).whenPressed(() -> selectedPixelStack = (selectedPixelStack - 1) % N_PIXEL_TAGS);
+        gamepad.getGamepadButton(GamepadKeys.Button.DPAD_RIGHT).whenPressed(() -> selectedPixelStack = (selectedPixelStack + 1) % N_PIXEL_TAGS);
     }
 
     @Override
     public void loop() {
         AutomaticAlignmentToPixels.goToPixelStack(selectedPixelStack);
+
+        telemetry.addLine(AutomaticAlignmentToPixels.isMoving() ? "Moving" : "Idle");
+        telemetry.addData("Selected pixel stack", selectedPixelStack);
     }
 
     @Override
