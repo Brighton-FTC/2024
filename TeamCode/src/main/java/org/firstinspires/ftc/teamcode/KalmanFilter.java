385a1aea4b39274139986c807241d6cba99d708f
la-reine-c <lawrence.tn.carter@icloud.com>
2023-11-28 18:04:51 +0000

    Added aeroplane launcher

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/KalmanFilter.java
@@ -0,0 +1,15 @@
+package org.firstinspires.ftc.teamcode;
+
+public class KalmanFilter {
+    void init() {
+        int y = 0;
+    }
+
+    int predictionStep() {
+        return 0;
+    }
+
+    int updateStep() {
+        return 0;
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PaperAeroplane.java
@@ -0,0 +1,38 @@
+package org.firstinspires.ftc.teamcode;
+
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.gamepad1;
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
+
+import com.arcrobotics.ftclib.gamepad.GamepadEx;
+import com.arcrobotics.ftclib.hardware.motors.Motor;
+import com.arcrobotics.ftclib.hardware.motors.MotorEx;
+
+public class PaperAeroplane {
+    private GamepadEx driverOp = new GamepadEx(gamepad1);
+    Motor aeroplaneMotor;
+    public PaperAeroplane(MotorEx paperAeroplaneMotor) {
+        aeroplaneMotor = paperAeroplaneMotor;
+
+    }
+    public void launch() {
+        aeroplaneMotor.setRunMode(Motor.RunMode.PositionControl);
+        aeroplaneMotor.setPositionCoefficient(0.05);
+        double kP = aeroplaneMotor.getPositionCoefficient();
+        aeroplaneMotor.setTargetPosition(2000);
+
+        aeroplaneMotor.set(0);
+
+        aeroplaneMotor.setPositionTolerance(13.6);
+        while (!aeroplaneMotor.atTargetPosition()) {
+            aeroplaneMotor.set(0.75);
+        }
+        aeroplaneMotor.stopMotor();
+        aeroplaneMotor.setDistancePerPulse(0.015);
+
+        aeroplaneMotor.setTargetDistance(18.0);
+
+        aeroplaneMotor.set(0.5);
+    }
+
+
+}
