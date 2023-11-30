3e3549d141d9791cea7d0cf21f42b0a8f2d440a9
12sliu <12sliu@brightoncollege.net>
2023-11-30 08:56:56 +0000

    more work on teleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/GenericTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/GenericTeleOp.java
@@ -1,14 +1,18 @@
 package org.firstinspires.ftc.teamcode.teleop;
 
-import com.arcrobotics.ftclib.command.InstantCommand;
+import com.arcrobotics.ftclib.drivebase.MecanumDrive;
 import com.arcrobotics.ftclib.gamepad.GamepadEx;
 import com.arcrobotics.ftclib.gamepad.GamepadKeys;
 import com.arcrobotics.ftclib.hardware.ServoEx;
 import com.arcrobotics.ftclib.hardware.SimpleServo;
+import com.arcrobotics.ftclib.hardware.motors.Motor;
 import com.arcrobotics.ftclib.hardware.motors.MotorEx;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
 import org.firstinspires.ftc.teamcode.PSButtons;
+import org.firstinspires.ftc.teamcode.components.test.ArmComponent;
+import org.firstinspires.ftc.teamcode.components.test.DroneLauncherComponent;
+import org.firstinspires.ftc.teamcode.components.test.GrabberComponent;
 
 public class GenericTeleOp extends OpMode {
 
@@ -18,40 +22,72 @@ public class GenericTeleOp extends OpMode {
     public static final GamepadKeys.Button OPERATION_VALOUR_BUTTON = PSButtons.CROSS;
 
     // P2 Controls
-
-    public static final GamepadKeys.Button TOGGLE_LIFT_BUTTON = PSButtons.CROSS;
+    public static final GamepadKeys.Button TOGGLE_ARM_BUTTON = PSButtons.CROSS;
     public static final GamepadKeys.Button TOGGLE_GRABBER_BUTTON = PSButtons.CIRCLE;
+    public static final GamepadKeys.Button DRONE_LAUNCH_BUTTON = PSButtons.SQUARE;
 
 
-    private GamepadEx playerGamepad1;
-    private GamepadEx playerGamepad2;
+    private GamepadEx player1Gamepad;
+    private GamepadEx player2Gamepad;
 
-    private ServoEx grabberServo;
+    private ServoEx grabberServo1;
+    private ServoEx grabberServo2;
     private GrabberComponent grabber;
 
-    private ArmComponent arm;
     private MotorEx armMotor;
     private ServoEx grabberRotatorServo;
+    private ArmComponent arm;
+
+    private MotorEx droneMotor;
+    private DroneLauncherComponent droneLauncher;
+
+    private MecanumDrive mecanumDrive;
+
 
     @Override
     public void init() {
-        playerGamepad1 = new GamepadEx(gamepad1);
-        playerGamepad2 = new GamepadEx(gamepad2);
+        player1Gamepad = new GamepadEx(gamepad1);
+        player2Gamepad = new GamepadEx(gamepad2);
 
-        grabberServo = new SimpleServo(hardwareMap, "grabber_servo",
+        grabberServo1 = new SimpleServo(hardwareMap, "grabber_servo",
                 GrabberComponent.GRABBER_CLOSED_POSITION,
                 GrabberComponent.GRABBER_OPEN_POSITION);
-        grabber = new GrabberComponent(grabberServo);
+        grabberServo2 = new SimpleServo(hardwareMap, "grabber_servo",
+                GrabberComponent.GRABBER_CLOSED_POSITION,
+                GrabberComponent.GRABBER_OPEN_POSITION);
 
         armMotor = new MotorEx(hardwareMap, "arm_motor");
-        grabberRotatorServo = new SimpleServo(hardwareMap, "grabber_rotator_servo");
-        arm = new ArmComponent(armMotor);
+        grabberRotatorServo = new SimpleServo(hardwareMap, "grabber_rotator_servo",
+                ArmComponent.GRABBER_TILT_DOWN_POSITION,
+                ArmComponent.GRABBER_TILT_UP_POSITION);
 
-        playerGamepad2.getGamepadButton(TOGGLE_GRABBER_BUTTON).whenPressed(GrabberComponent::toggle);
+        droneLauncher = new DroneLauncherComponent()
+
+        mecanumDrive = new MecanumDrive(
+                new Motor(hardwareMap, "frontLeftDrive"),
+                new Motor(hardwareMap, "frontRightDrive"),
+                new Motor(hardwareMap, "backLeftDrive"),
+                new Motor(hardwareMap, "backRightDrive"));
+
+
+
+        arm = new ArmComponent(armMotor, grabberRotatorServo);
+        grabber = new GrabberComponent(grabberServo1, grabberServo2);
+
+
+        player2Gamepad.getGamepadButton(TOGGLE_GRABBER_BUTTON).whenPressed(grabber::toggle);
+        player2Gamepad.getGamepadButton(TOGGLE_ARM_BUTTON).whenPressed(arm::toggle);
+        player2Gamepad.getGamepadButton(DRONE_LAUNCH_BUTTON).whenPressed(arm::toggle);
     }
 
     @Override
     public void loop() {
+        double leftY = player1Gamepad.getLeftY();
+        double leftX = player1Gamepad.getLeftX();
+        double rightX = player1Gamepad.getRightX();
 
+        mecanumDrive.driveRobotCentric(leftX, leftY, rightX,false);
+
+        telemetry.update();
     }
 }
