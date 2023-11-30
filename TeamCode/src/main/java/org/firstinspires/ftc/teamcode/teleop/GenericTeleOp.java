3507a0220b14156dc883c21c0fd4aa3bda78e41d
12sliu <12sliu@brightoncollege.net>
2023-11-30 14:40:33 +0000

    finish basic teleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/GenericTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/GenericTeleOp.java
@@ -8,23 +8,31 @@ import com.arcrobotics.ftclib.hardware.SimpleServo;
 import com.arcrobotics.ftclib.hardware.motors.Motor;
 import com.arcrobotics.ftclib.hardware.motors.MotorEx;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.Range;
 
 import org.firstinspires.ftc.teamcode.PSButtons;
 import org.firstinspires.ftc.teamcode.components.test.ArmComponent;
 import org.firstinspires.ftc.teamcode.components.test.DroneLauncherComponent;
 import org.firstinspires.ftc.teamcode.components.test.GrabberComponent;
 
+@TeleOp(name = "TeleOp", group = "teleop-test")
 public class GenericTeleOp extends OpMode {
 
     // P1 Controls
+    public static final GamepadKeys.Button DPAD_STRAFE_LEFT = GamepadKeys.Button.DPAD_LEFT;
+    public static final GamepadKeys.Button DPAD_STRAFE_RIGHT = GamepadKeys.Button.DPAD_RIGHT;
+    public static final GamepadKeys.Button DPAD_FORWARD = GamepadKeys.Button.DPAD_UP;
+    public static final GamepadKeys.Button DPAD_BACKWARDS = GamepadKeys.Button.DPAD_DOWN;
 
-//    public static final GamepadKeys.Button OPERATION_VALOUR_BUTTON = GamepadKeys.Button.LEFT_BUMPER;
-    public static final GamepadKeys.Button OPERATION_VALOUR_BUTTON = PSButtons.CROSS;
 
     // P2 Controls
     public static final GamepadKeys.Button TOGGLE_ARM_BUTTON = PSButtons.CROSS;
     public static final GamepadKeys.Button TOGGLE_GRABBER_BUTTON = PSButtons.CIRCLE;
-    public static final GamepadKeys.Button DRONE_LAUNCH_BUTTON = PSButtons.SQUARE;
+    public static final GamepadKeys.Button DRONE_LAUNCH_1_BUTTON = GamepadKeys.Button.LEFT_BUMPER;
+    public static final GamepadKeys.Button DRONE_LAUNCH_2_BUTTON = GamepadKeys.Button.RIGHT_BUMPER;
+
+
 
 
     private GamepadEx player1Gamepad;
@@ -38,7 +46,7 @@ public class GenericTeleOp extends OpMode {
     private ServoEx grabberRotatorServo;
     private ArmComponent arm;
 
-    private MotorEx droneMotor;
+    private ServoEx droneServo;
     private DroneLauncherComponent droneLauncher;
 
     private MecanumDrive mecanumDrive;
@@ -61,7 +69,9 @@ public class GenericTeleOp extends OpMode {
                 ArmComponent.GRABBER_TILT_DOWN_POSITION,
                 ArmComponent.GRABBER_TILT_UP_POSITION);
 
-        droneLauncher = new DroneLauncherComponent()
+        droneServo = new SimpleServo(hardwareMap, "drone_servo",
+                DroneLauncherComponent.READY_POSITION,
+                DroneLauncherComponent.LAUNCH_POSITION);
 
         mecanumDrive = new MecanumDrive(
                 new Motor(hardwareMap, "frontLeftDrive"),
@@ -70,14 +80,13 @@ public class GenericTeleOp extends OpMode {
                 new Motor(hardwareMap, "backRightDrive"));
 
 
-
         arm = new ArmComponent(armMotor, grabberRotatorServo);
         grabber = new GrabberComponent(grabberServo1, grabberServo2);
+        droneLauncher = new DroneLauncherComponent(droneServo);
 
 
         player2Gamepad.getGamepadButton(TOGGLE_GRABBER_BUTTON).whenPressed(grabber::toggle);
         player2Gamepad.getGamepadButton(TOGGLE_ARM_BUTTON).whenPressed(arm::toggle);
-        player2Gamepad.getGamepadButton(DRONE_LAUNCH_BUTTON).whenPressed(arm::toggle);
     }
 
     @Override
@@ -86,7 +95,27 @@ public class GenericTeleOp extends OpMode {
         double leftX = player1Gamepad.getLeftX();
         double rightX = player1Gamepad.getRightX();
 
+        if (player1Gamepad.getButton(DPAD_STRAFE_LEFT)){
+            leftX += 0.75;
+        }
+        if (player1Gamepad.getButton(DPAD_STRAFE_RIGHT)){
+            leftX -= 0.75;
+        }
+        leftX = Range.clip(leftX, -1, 1);
+
+        if (player1Gamepad.getButton(DPAD_FORWARD)){
+            leftY += 0.75;
+        }
+        if (player1Gamepad.getButton(DPAD_BACKWARDS)){
+            leftY -= 0.75;
+        }
+        leftY = Range.clip(leftY, -1, 1);
+
         mecanumDrive.driveRobotCentric(leftX, leftY, rightX,false);
+
+        if (player1Gamepad.getButton(DRONE_LAUNCH_1_BUTTON) || player1Gamepad.getButton(DRONE_LAUNCH_2_BUTTON)){
+            droneLauncher.launch();
+        }
 
         telemetry.update();
     }
