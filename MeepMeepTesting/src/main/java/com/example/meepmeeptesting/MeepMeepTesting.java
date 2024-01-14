b0c9e76f9256eeb42488c20669e7c5c35a80e99d
Saku <saku.bottingota@gmail.com>
2024-01-14 17:42:18 +0000

    Made trajectories, and changed some values in TrajectoryPositions.

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -1,34 +1,150 @@
 package com.example.meepmeeptesting;
 
+import static com.example.meepmeeptesting.TrajectoryPositions.*;
+
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
+import com.noahbres.meepmeep.roadrunner.DriveShim;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
+import com.noahbres.meepmeep.roadrunner.trajectorysequence.TrajectorySequence;
+import com.noahbres.meepmeep.roadrunner.trajectorysequence.TrajectorySequenceBuilder;
+
+import org.jetbrains.annotations.NotNull;
 
 public class MeepMeepTesting {
     public static void main(String[] args) {
-        MeepMeep meepMeep = new MeepMeep(800);
+        MeepMeep meepMeep = new MeepMeep(750);
 
-        RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
-                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
-                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
-                .followTrajectorySequence(drive ->
-                        drive.trajectorySequenceBuilder(new Pose2d(0, 0, 0))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
-                                .build()
-                );
+
+        for (int i = 0; i < 12; i++) {
+            int finalI = i;
+            RoadRunnerBotEntity bot = new DefaultBotBuilder(meepMeep)
+                    .setDimensions(15, 15)
+//                    .setConstraints(new Constraints()) // TODO: set constraints
+                    .followTrajectorySequence(drive -> buildTrajectory(drive, finalI, 1));
+            meepMeep.addEntity(bot);
+        }
 
         meepMeep.setBackground(MeepMeep.Background.FIELD_CENTERSTAGE_JUICE_DARK)
                 .setDarkMode(true)
                 .setBackgroundAlpha(0.95f)
-                .addEntity(myBot)
                 .start();
+    }
+
+    /**
+     * Builds robot trajectory. Example use: <br />
+     * <code>
+     * defaultBotBuilder.followTrajectory(drive -> buildTrajectory(drive, 0, 3));
+     * </code>
+     *
+     * @param drive             The {@link DriveShim} object.
+     * @param robotId           Which path the robot follows, from 0 to 11 (inclusive).
+     * @param pixelCollectTimes Make the robot collect a pixel this many times (for far starting side),
+     *                          or this many times plus one (for audience starting side).
+     * @return A {@link TrajectorySequence} object, containing the robot's trajectory.
+     */
+    @NotNull
+    private static TrajectorySequence buildTrajectory(@NotNull DriveShim drive, int robotId, int pixelCollectTimes) {
+        if (robotId < 0 || robotId > 11) {
+            throw new IllegalArgumentException("Parameter 'robotId' must be between 0 and 11 (inclusive).");
+        }
+
+        Pose2d startPose;
+        if (robotId < 3) {
+            startPose = STARTING_POSE_RED_AUDIENCE;
+        } else if (robotId < 6) {
+            startPose = STARTING_POSE_RED_FAR;
+        } else if (robotId < 9) {
+            startPose = STARTING_POSE_BLUE_AUDIENCE;
+        } else {
+            startPose = STARTING_POSE_BLUE_FAR;
+        }
+        TrajectorySequenceBuilder trajectory = drive.trajectorySequenceBuilder(startPose);
+
+        Pose2d spikeMarkPose = new Pose2d();
+        Pose2d backdropPose = new Pose2d();
+
+        switch (robotId) {
+            case 0:
+                spikeMarkPose = SPIKE_MARK_RED_AUDIENCE_LEFT;
+                backdropPose = BACKDROP_RED_LEFT;
+                break;
+
+            case 1:
+                spikeMarkPose = SPIKE_MARK_RED_AUDIENCE_CENTER;
+                backdropPose = BACKDROP_RED_CENTER;
+                break;
+
+            case 2:
+                spikeMarkPose = SPIKE_MARK_RED_AUDIENCE_RIGHT;
+                backdropPose = BACKDROP_RED_RIGHT;
+                break;
+
+            case 3:
+                spikeMarkPose = SPIKE_MARK_RED_FAR_LEFT;
+                backdropPose = BACKDROP_RED_LEFT;
+                break;
+
+            case 4:
+                spikeMarkPose = SPIKE_MARK_RED_FAR_CENTER;
+                backdropPose = BACKDROP_RED_CENTER;
+                break;
+
+            case 5:
+                spikeMarkPose = SPIKE_MARK_RED_FAR_RIGHT;
+                backdropPose = BACKDROP_RED_RIGHT;
+                break;
+
+            case 6:
+                spikeMarkPose = SPIKE_MARK_BLUE_AUDIENCE_LEFT;
+                backdropPose = BACKDROP_BLUE_LEFT;
+                break;
+
+            case 7:
+                spikeMarkPose = SPIKE_MARK_BLUE_AUDIENCE_CENTER;
+                backdropPose = BACKDROP_BLUE_CENTER;
+                break;
+
+            case 8:
+                spikeMarkPose = SPIKE_MARK_BLUE_AUDIENCE_RIGHT;
+                backdropPose = BACKDROP_BLUE_RIGHT;
+                break;
+
+            case 9:
+                spikeMarkPose = SPIKE_MARK_BLUE_FAR_LEFT;
+                backdropPose = BACKDROP_BLUE_LEFT;
+                break;
+
+            case 10:
+                spikeMarkPose = SPIKE_MARK_BLUE_FAR_CENTER;
+                backdropPose = BACKDROP_BLUE_CENTER;
+                break;
+
+            case 11:
+                spikeMarkPose = SPIKE_MARK_BLUE_FAR_RIGHT;
+                backdropPose = BACKDROP_BLUE_RIGHT;
+        }
+
+        trajectory.splineTo(spikeMarkPose.vec(), spikeMarkPose.getHeading());
+
+        Pose2d pixelStackPose = robotId >= 6 ? PIXEL_STACK_LEFT_1 : PIXEL_STACK_RIGHT_1;
+
+
+        if ((robotId < 3) || (robotId >= 6 && robotId < 9)) {
+            trajectory.lineToLinearHeading(pixelStackPose);
+            trajectory.addDisplacementMarker(() -> {/* pick up pixel */});
+        }
+
+        trajectory.lineToLinearHeading(backdropPose);
+
+        for (int i = 0; i < pixelCollectTimes; i++) {
+            trajectory.addDisplacementMarker(() -> {/* place pixel */});
+            trajectory.lineToLinearHeading(pixelStackPose);
+            trajectory.addDisplacementMarker(() -> {/* pick up pixel */});
+            trajectory.lineToLinearHeading(backdropPose);
+        }
+
+        return trajectory.build();
     }
 }
--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/TrajectoryPositions.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/TrajectoryPositions.java
@@ -6,41 +6,41 @@ import com.acmerobotics.roadrunner.geometry.Pose2d;
  * Class containing positions of useful places on the field.
  */
 public final class TrajectoryPositions {
-    public static final Pose2d STARTING_POSE_RED_AUDIENCE = new Pose2d(-35, -60, 90);
-    public static final Pose2d STARTING_POSE_RED_FAR = new Pose2d(10, -60, 90);
+    public static final Pose2d STARTING_POSE_RED_AUDIENCE = new Pose2d(-35, -60, Math.toRadians(90));
+    public static final Pose2d STARTING_POSE_RED_FAR = new Pose2d(10, -60, Math.toRadians(90));
 
-    public static final Pose2d STARTING_POSE_BLUE_AUDIENCE = new Pose2d(-35, 60, 270);
-    public static final Pose2d STARTING_POSE_BLUE_FAR = new Pose2d(10, 60, 270);
+    public static final Pose2d STARTING_POSE_BLUE_AUDIENCE = new Pose2d(-35, 60, Math.toRadians(270));
+    public static final Pose2d STARTING_POSE_BLUE_FAR = new Pose2d(10, 60, Math.toRadians(270));
 
-    public static final Pose2d SPIKE_MARK_RED_AUDIENCE_LEFT = new Pose2d(-35, -30, 180);
-    public static final Pose2d SPIKE_MARK_RED_AUDIENCE_CENTER = new Pose2d(-35, -30, 90);
-    public static final Pose2d SPIKE_MARK_RED_AUDIENCE_RIGHT = new Pose2d(-35, -30, 0);
+    public static final Pose2d SPIKE_MARK_RED_AUDIENCE_LEFT = new Pose2d(-35, -30, Math.toRadians(180));
+    public static final Pose2d SPIKE_MARK_RED_AUDIENCE_CENTER = new Pose2d(-35, -30, Math.toRadians(90));
+    public static final Pose2d SPIKE_MARK_RED_AUDIENCE_RIGHT = new Pose2d(-35, -30, Math.toRadians(0));
 
-    public static final Pose2d SPIKE_MARK_RED_FAR_LEFT = new Pose2d(10, -30, 180);
-    public static final Pose2d SPIKE_MARK_RED_FAR_CENTER = new Pose2d(10, -30, 90);
-    public static final Pose2d SPIKE_MARK_RED_FAR_RIGHT = new Pose2d(10, -30, 0);
+    public static final Pose2d SPIKE_MARK_RED_FAR_LEFT = new Pose2d(10, -30, Math.toRadians(180));
+    public static final Pose2d SPIKE_MARK_RED_FAR_CENTER = new Pose2d(10, -30, Math.toRadians(90));
+    public static final Pose2d SPIKE_MARK_RED_FAR_RIGHT = new Pose2d(10, -30, Math.toRadians(0));
 
-    public static final Pose2d SPIKE_MARK_BLUE_AUDIENCE_LEFT = new Pose2d(-35, 30, 0);
-    public static final Pose2d SPIKE_MARK_BLUE_AUDIENCE_CENTER = new Pose2d(-35, 30, 270);
-    public static final Pose2d SPIKE_MARK_BLUE_AUDIENCE_RIGHT = new Pose2d(-35, 30, 180);
+    public static final Pose2d SPIKE_MARK_BLUE_AUDIENCE_LEFT = new Pose2d(-35, 30, Math.toRadians(0));
+    public static final Pose2d SPIKE_MARK_BLUE_AUDIENCE_CENTER = new Pose2d(-35, 30, Math.toRadians(270));
+    public static final Pose2d SPIKE_MARK_BLUE_AUDIENCE_RIGHT = new Pose2d(-35, 30, Math.toRadians(180));
 
-    public static final Pose2d SPIKE_MARK_BLUE_FAR_LEFT = new Pose2d(10, 30, 0);
-    public static final Pose2d SPIKE_MARK_BLUE_FAR_CENTER = new Pose2d(10, 30, 270);
-    public static final Pose2d SPIKE_MARK_BLUE_FAR_RIGHT = new Pose2d(10, 30, 180);
+    public static final Pose2d SPIKE_MARK_BLUE_FAR_LEFT = new Pose2d(10, 30, Math.toRadians(0));
+    public static final Pose2d SPIKE_MARK_BLUE_FAR_CENTER = new Pose2d(10, 30, Math.toRadians(270));
+    public static final Pose2d SPIKE_MARK_BLUE_FAR_RIGHT = new Pose2d(10, 30, Math.toRadians(180));
 
-    public static final Pose2d BACKDROP_RED_LEFT = new Pose2d(60, -30, 0);
-    public static final Pose2d BACKDROP_RED_CENTER = new Pose2d(60, -35, 0);
-    public static final Pose2d BACKDROP_RED_RIGHT = new Pose2d(60, -40, 0);
+    public static final Pose2d BACKDROP_RED_LEFT = new Pose2d(60, -30, Math.toRadians(180));
+    public static final Pose2d BACKDROP_RED_CENTER = new Pose2d(60, -35, Math.toRadians(180));
+    public static final Pose2d BACKDROP_RED_RIGHT = new Pose2d(60, -40, Math.toRadians(180));
 
-    public static final Pose2d BACKDROP_BLUE_LEFT = new Pose2d(60, 30, 0);
-    public static final Pose2d BACKDROP_BLUE_CENTER = new Pose2d(60, 35, 0);
-    public static final Pose2d BACKDROP_BLUE_RIGHT = new Pose2d(60, 40, 0);
+    public static final Pose2d BACKDROP_BLUE_LEFT = new Pose2d(60, 30, Math.toRadians(180));
+    public static final Pose2d BACKDROP_BLUE_CENTER = new Pose2d(60, 35, Math.toRadians(180));
+    public static final Pose2d BACKDROP_BLUE_RIGHT = new Pose2d(60, 40, Math.toRadians(180));
 
-    public static final Pose2d PIXEL_STACK_LEFT_0 = new Pose2d(-60, 35, 180);
-    public static final Pose2d PIXEL_STACK_LEFT_1 = new Pose2d(-60, 23, 180);
-    public static final Pose2d PIXEL_STACK_LEFT_2 = new Pose2d(-60, 10, 180);
+    public static final Pose2d PIXEL_STACK_LEFT_0 = new Pose2d(-60, 35, Math.toRadians(180));
+    public static final Pose2d PIXEL_STACK_LEFT_1 = new Pose2d(-60, 23, Math.toRadians(180));
+    public static final Pose2d PIXEL_STACK_LEFT_2 = new Pose2d(-60, 10, Math.toRadians(180));
 
-    public static final Pose2d PIXEL_STACK_RIGHT_0 = new Pose2d(-60, -10, 180);
-    public static final Pose2d PIXEL_STACK_RIGHT_1 = new Pose2d(-60, -23, 180);
-    public static final Pose2d PIXEL_STACK_RIGHT_2 = new Pose2d(-60, -35, 180);
+    public static final Pose2d PIXEL_STACK_RIGHT_0 = new Pose2d(-60, -35, Math.toRadians(180));
+    public static final Pose2d PIXEL_STACK_RIGHT_1 = new Pose2d(-60, -23, Math.toRadians(180));
+    public static final Pose2d PIXEL_STACK_RIGHT_2 = new Pose2d(-60, -10, Math.toRadians(180));
 }
