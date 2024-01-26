0b24c0f08e0a78db11e8650aa9cab0a7ffde5e58
12sliu <12sliu@brightoncollege.net>
2024-01-26 17:52:44 +0000

    added voltage normalization, look in functionalitytester for how to use

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/components/test/ArmFunctionalityTester.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/components/test/ArmFunctionalityTester.java
@@ -3,11 +3,15 @@ package org.firstinspires.ftc.teamcode.components.test;
 import com.arcrobotics.ftclib.gamepad.GamepadEx;
 import com.arcrobotics.ftclib.hardware.SimpleServo;
 import com.arcrobotics.ftclib.hardware.motors.MotorEx;
+import com.qualcomm.hardware.lynx.LynxModule;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import org.firstinspires.ftc.robotcore.external.navigation.VoltageUnit;
 import org.firstinspires.ftc.teamcode.util.cachinghardwaredevice.cachingftclib.FTCLibCachingMotorEx;
 import org.firstinspires.ftc.teamcode.util.inputs.PSButtons;
+
+import java.util.List;
 
 /**
  * Code to test the functionality of the arm. <br />
@@ -25,8 +29,17 @@ public class ArmFunctionalityTester extends OpMode {
 
     @Override
     public void init() {
+        List<LynxModule> allHubs = hardwareMap.getAll(LynxModule.class);
+
+        for (LynxModule module : allHubs) {
+            module.setBulkCachingMode(LynxModule.BulkCachingMode.MANUAL);
+        }
+
+        // this is how you get the voltage - just takes first hub
+        // not sure if you should be getting average of all voltages but prob doesn't matter
         armComponent = new ArmComponent(
-                new FTCLibCachingMotorEx(hardwareMap, "arm_motor")
+                new FTCLibCachingMotorEx(hardwareMap, "arm_motor"),
+                allHubs.get(0).getInputVoltage(VoltageUnit.VOLTS)
         );
 
         gamepad = new GamepadEx(gamepad1);
