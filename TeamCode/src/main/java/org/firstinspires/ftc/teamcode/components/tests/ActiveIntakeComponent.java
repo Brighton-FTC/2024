f68994c5fdab8aa3ebcac4a4daa52156abf19a91
bouncy2019 <144822349+bouncy2019@users.noreply.github.com>
2024-02-18 12:27:08 +0000

    Update ActiveIntakeComponent.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/components/tests/ActiveIntakeComponent.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/components/tests/ActiveIntakeComponent.java
@@ -4,25 +4,32 @@ import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.har
 
 import com.arcrobotics.ftclib.hardware.ServoEx;
 import com.arcrobotics.ftclib.hardware.SimpleServo;
+import com.arcrobotics.ftclib.hardware.motors.MotorEx;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
 public class ActiveIntakeComponent {
     private boolean motorState = false;
     private double motorSpeed = 0.9;
     private int servoAngle = 0;
-    MotorEx motor = new MotorEx(hardwareMap, "motor");
-    ServoEx left_servo = new SimpleServo(hardwareMap, "left_servo", 0, 360);
-    ServoEx right_servo = new SimpleServo(hardwareMap, "right_servo", 0, 360);
+    MotorEx motor;
+    ServoEx leftServo;
+    ServoEx rightServo;
+
+    public ActiveIntakeComponent(MotorEx motor,ServoEx leftServo,ServoEx rightServo) {
+        this.motor = motor;
+        this.leftServo = leftServo;
+        this.rightServo = rightServo;
+    }
 
 
 
     public void turnMotorOn() {
         motorState = true;
-        motor.setVelocity(motorSpeed);
+        motor.set(motorSpeed);
     }
     public void turnMotorOff() {
         motorState = false;
-        setVelocity(0);
+        motor.set(0);
     }
     public void toggleMotor() {
         if (motorState) {
@@ -34,8 +41,8 @@ public class ActiveIntakeComponent {
 
     public void rotateServosForStack(int pixels) {
         servoAngle = 180 * pixels;
-        left_servo.rotateByAngle(180);
-        right_servo.rotateByAngle(-180);
+        leftServo.rotateByAngle(180);
+        rightServo.rotateByAngle(-180);
     }
     public void rotateServosManually() {
         servoAngle += 180;
