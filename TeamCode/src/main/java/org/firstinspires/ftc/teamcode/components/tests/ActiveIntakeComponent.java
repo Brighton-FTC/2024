bcfbc31e3334b7bb00b37d421876e0dbde6059c7
bouncy2019 <144822349+bouncy2019@users.noreply.github.com>
2024-02-18 12:11:58 +0000

    Update ActiveIntakeComponent.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/components/tests/ActiveIntakeComponent.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/components/tests/ActiveIntakeComponent.java
@@ -1,40 +1,43 @@
 package org.firstinspires.ftc.teamcode.components.tests;
-import com.arcrobotics.ftclib.hardware.motors.Motor;
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.util.Range;
+
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
+
+import com.arcrobotics.ftclib.hardware.ServoEx;
+import com.arcrobotics.ftclib.hardware.SimpleServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
 public class ActiveIntakeComponent {
-        private boolean motorState = false;
-        private double motorSpeed = 0.9;
-        private int servoAngle = 0;
-        DcMotor dcMotor = hardwareMap.dcMotor.get("motor");
-        Servo servo = hardwareMap.servo.get("left_servo");
-        Servo servo = harwareMap.servo.get("right_servo");
+    private boolean motorState = false;
+    private double motorSpeed = 0.9;
+    private int servoAngle = 0;
+    MotorEx motor = new MotorEx(hardwareMap, "motor");
+    ServoEx left_servo = new SimpleServo(hardwareMap, "left_servo", 0, 360);
+    ServoEx right_servo = new SimpleServo(hardwareMap, "right_servo", 0, 360);
 
 
-        public void turnMotorOn() {
-            motorState = true;
-            setVelocity(motorSpeed);
-        }
-        public void turnMotorOff() {
-            motorState = false;
-            setVelocity(double 0);
-        }
-        public void toggleMotor() {
-            if (motorState) {
-                turnMotorOff();
-            } else {
-                turnMotorOn();
-            }
-        }
 
-        public void rotateServosForStack(int pixels) {
-            servoAngle = 180 * pixels;
-            servo.rotateByAngle(180);
-            servo.rotateByAngle(-180);
+    public void turnMotorOn() {
+        motorState = true;
+        motor.setVelocity(motorSpeed);
+    }
+    public void turnMotorOff() {
+        motorState = false;
+        setVelocity(0);
+    }
+    public void toggleMotor() {
+        if (motorState) {
+            turnMotorOff();
+        } else {
+            turnMotorOn();
         }
-        public void rotateServosManually() {
-            servoAngle += 180;
-        }
+    }
+
+    public void rotateServosForStack(int pixels) {
+        servoAngle = 180 * pixels;
+        left_servo.rotateByAngle(180);
+        right_servo.rotateByAngle(-180);
+    }
+    public void rotateServosManually() {
+        servoAngle += 180;
     }
 }
