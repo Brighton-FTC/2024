3643871e9be555e1a3cc8622a20e0cea40d45d1b
12sliu <12sliu@brightoncollege.net>
2024-06-21 08:20:53 +0100

    park

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/autonomous/AutonomousPark.java
@@ -0,0 +1,30 @@
+package org.firstinspires.ftc.teamcode.opModes.autonomous;
+
+import com.arcrobotics.ftclib.hardware.motors.MotorEx;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+@Autonomous(name = "park")
+public class AutonomousPark extends OpMode {
+    public MotorEx backLeftMotor;
+    public MotorEx backRightMotor;
+    public MotorEx frontLeftMotor;
+    public MotorEx frontRightMotor;
+
+
+    @Override
+    public void init() {
+        frontLeftMotor = new MotorEx(hardwareMap,"front_left_drive");
+        frontRightMotor = new MotorEx(hardwareMap,"front_right_drive");
+        backLeftMotor = new MotorEx(hardwareMap,"back_left_drive");
+        backRightMotor = new MotorEx(hardwareMap,"back_right_drive");
+    }
+
+    @Override
+    public void loop() {
+        frontLeftMotor.set(0.5);
+        frontRightMotor.set(0.5);
+        backLeftMotor.set(0.5);
+        backRightMotor.set(0.5);
+    }
+}
